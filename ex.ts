d6fa37fdf4ac8fb2d8a349de1dea260a5fd8a2ea
Gustavo Raimo <gustavo.raimo92@gmail.com>
2023-12-04 22:56:57 -0300

    Exercicio Typescript M26

---
+++ ex.ts
@@ -0,0 +1,12 @@
+function multiplicacao(num1: number, num2:number){
+    return num1 * num2;
+}
+
+function saudacao(nome: string){
+    return "Olá " + nome;
+}
+
+
+console.log(multiplicacao(2,14))
+
+console.log(saudacao('Gustavo'))
